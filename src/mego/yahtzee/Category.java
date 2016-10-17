7113ec43b526ef5f65aa3b1790aa05aef477658d
mego <mega.gamer05@gmail.com>
2016-10-16 23:49:13 -0500

    Utility methods for matching categories from dice

--- src/mego/yahtzee/Category.java
+++ src/mego/yahtzee/Category.java
@@ -1,5 +1,10 @@
 package mego.yahtzee;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public enum Category {
 	ACES,
 	TWOS,
@@ -31,5 +36,32 @@ public enum Category {
 	
 	public boolean isLower() {
 		return !this.isUpper();
+	}
+	
+	public boolean matches(int[] dice) {
+		switch(this) {
+		case THREE_OF_A_KIND:
+			return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() >= 3;
+		case FOUR_OF_A_KIND:
+			return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() >= 4;
+		case YAHTZEE:
+			return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() == 5;
+		case FULL_HOUSE:
+			int[] distinct = IntStream.of(dice).distinct().toArray();
+			int[] counts = IntStream.of(distinct).map(die -> Util.count(Util.boxIntArray(dice), die)).sorted().toArray();
+			return counts[0] == 2 && counts[1] == 3;
+		case SMALL_STRAIGHT:
+			int[][] small_straights = {{1,2,3,4},{2,3,4,5},{3,4,5,6}};
+			return Stream.of(small_straights).anyMatch(s -> Collections.indexOfSubList(Arrays.asList(Stream.of(Util.boxIntArray(dice)).distinct().sorted().toArray()), Arrays.asList(Stream.of(Util.boxIntArray(s)).toArray())) != -1);
+		case LARGE_STRAIGHT:
+			int[][] large_straights = {{1,2,3,4,5},{2,3,4,5,6}};
+			return Stream.of(large_straights).anyMatch(s -> Collections.indexOfSubList(Arrays.asList(Stream.of(Util.boxIntArray(dice)).distinct().sorted().toArray()), Arrays.asList(Stream.of(Util.boxIntArray(s)).toArray())) != -1);
+		default:
+			return true;
+		}
+	}
+	
+	public static Category[] getMatchingCategories(int[] dice) {
+		return Stream.of(Category.values()).filter(c -> c.matches(dice)).toArray(Category[]::new);
 	}
 }
