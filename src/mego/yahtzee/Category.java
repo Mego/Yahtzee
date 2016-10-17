07a2e3c20784cd2af64affcb8aab6aba04d17205
mego <mega.gamer05@gmail.com>
2016-10-17 14:55:32 -0500

    Add Category.getScore method, better formatting

--- src/mego/yahtzee/Category.java
+++ src/mego/yahtzee/Category.java
@@ -40,24 +40,54 @@ public enum Category {
 	
 	public boolean matches(int[] dice) {
 		switch(this) {
-		case THREE_OF_A_KIND:
-			return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() >= 3;
-		case FOUR_OF_A_KIND:
-			return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() >= 4;
-		case YAHTZEE:
-			return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() == 5;
-		case FULL_HOUSE:
-			int[] distinct = IntStream.of(dice).distinct().toArray();
-			int[] counts = IntStream.of(distinct).map(die -> Util.count(Util.boxIntArray(dice), die)).sorted().toArray();
-			return counts[0] == 2 && counts[1] == 3;
-		case SMALL_STRAIGHT:
-			int[][] small_straights = {{1,2,3,4},{2,3,4,5},{3,4,5,6}};
-			return Stream.of(small_straights).anyMatch(s -> Collections.indexOfSubList(Arrays.asList(Stream.of(Util.boxIntArray(dice)).distinct().sorted().toArray()), Arrays.asList(Stream.of(Util.boxIntArray(s)).toArray())) != -1);
-		case LARGE_STRAIGHT:
-			int[][] large_straights = {{1,2,3,4,5},{2,3,4,5,6}};
-			return Stream.of(large_straights).anyMatch(s -> Collections.indexOfSubList(Arrays.asList(Stream.of(Util.boxIntArray(dice)).distinct().sorted().toArray()), Arrays.asList(Stream.of(Util.boxIntArray(s)).toArray())) != -1);
-		default:
-			return true;
+			case THREE_OF_A_KIND:
+				return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() >= 3;
+			case FOUR_OF_A_KIND:
+				return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() >= 4;
+			case YAHTZEE:
+				return IntStream.of(dice).map(die -> Util.count(Util.boxIntArray(dice), die)).max().getAsInt() == 5;
+			case FULL_HOUSE:
+				int[] distinct = IntStream.of(dice).distinct().toArray();
+				int[] counts = IntStream.of(distinct).map(die -> Util.count(Util.boxIntArray(dice), die)).sorted().toArray();
+				return counts[0] == 2 && counts[1] == 3;
+			case SMALL_STRAIGHT:
+				int[][] small_straights = {{1,2,3,4},{2,3,4,5},{3,4,5,6}};
+				return Stream.of(small_straights).anyMatch(s -> Collections.indexOfSubList(Arrays.asList(Stream.of(Util.boxIntArray(dice)).distinct().sorted().toArray()), Arrays.asList(Stream.of(Util.boxIntArray(s)).toArray())) != -1);
+			case LARGE_STRAIGHT:
+				int[][] large_straights = {{1,2,3,4,5},{2,3,4,5,6}};
+				return Stream.of(large_straights).anyMatch(s -> Collections.indexOfSubList(Arrays.asList(Stream.of(Util.boxIntArray(dice)).distinct().sorted().toArray()), Arrays.asList(Stream.of(Util.boxIntArray(s)).toArray())) != -1);
+			default:
+				return true;
+			}
+	}
+	
+	public int getScore(int[] dice) {
+		if(this.matches(dice)) {
+			switch(this) {
+				case FULL_HOUSE:
+					return 25;
+				case SMALL_STRAIGHT:
+					return 30;
+				case LARGE_STRAIGHT:
+					return 40;
+				case YAHTZEE:
+					return 50;
+				case THREE_OF_A_KIND:
+				case FOUR_OF_A_KIND:
+				case CHANCE:
+					return IntStream.of(dice).sum();
+				case ACES:
+				case TWOS:
+				case THREES:
+				case FOURS:
+				case FIVES:
+				case SIXES:
+					return IntStream.of(dice).filter(die -> die == Util.upperCategoryToInt(this)).sum();
+				default: // this shouldn't be necessary but Java is dumb and can't tell that all the cases are covered
+					throw new IllegalArgumentException();
+			}
+		} else {
+			return 0;
 		}
 	}
 	
